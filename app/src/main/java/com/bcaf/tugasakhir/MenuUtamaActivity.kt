1b826478617426a48c89f8fa4b3b661359bd41fb
Rahmad Nur Azim <rahmad.azim@aux.dkatalis.com>
2023-01-12 10:34:46 +0700

    Initial commit

---
+++ app/src/main/java/com/bcaf/tugasakhir/MenuUtamaActivity.kt
@@ -0,0 +1,11 @@
+package com.bcaf.tugasakhir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuUtamaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
